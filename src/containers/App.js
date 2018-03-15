e42b6497863921b7acccd5b2ac23ba0758fcb5bb
Scott Choi <yhlivefor@gmail.com>
2018-03-15 23:51:03 +0900

    Add react-router-dom

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Home from './Home';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" component={Home}/>
+      </BrowserRouter>
     );
   }
 }
