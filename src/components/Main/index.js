00a795fa3b125c5509829380a6ec93e77864b18c
Scott Choi <yhlivefor@gmail.com>
2018-03-17 22:35:33 +0900

    Add webSpeechApi

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './style.css';
-import { Button } from 'antd';
+import { Button, Select } from 'antd';
+const Option = Select.Option;
 
-const Main = () => {
+const Main = ({ voices}) => {
+
+  const OptionView = voices.map((data, i) => (
+    <Option value={data.name}>{data.name}</Option>
+  ));
+
   return (
     <div className="main_layout">
       <div className="main_title">Today Sentence</div>
+      <Select defaultValue={voices[0]} style={{ width: 120 }}>
+        {OptionView}
+      </Select>
       <Button className="main_play_btn">Play</Button>
     </div>
   );
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Main from '../components/Main';
+const synth = window.speechSynthesis;
 
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      voices: []
+    }
+  }
+
+  componentDidMount(){
+    this.handleGetVoice();
+  }
+
+  handleGetVoice = () => {
+    let voices = synth.getVoices();
+    this.setState({voices});
+  }
+
+
   render () {
+    console.log(this.state.voices);
     return (
       <div>
-        <Main/>
+        <Main voices={this.state.voices}/>
+        <div>test</div>
       </div>
     )
   }
